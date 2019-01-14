24cdd0d5e96c87b8fb103d84a5df9e335d7b069a
newlls <523786175@qq.com>
2019-01-14 15:47:07 +0800

    入口

--- mall/src/App.vue
+++ mall/src/App.vue
@@ -1,31 +1,17 @@
 <template>
   <div id="app">
-    <van-tabbar v-model="active">
-      <van-tabbar-item icon="home-o" to="/">首页</van-tabbar-item>
-      <van-tabbar-item icon="comment-o" to="/classify">分类</van-tabbar-item>
-      <van-tabbar-item icon="cart-o" to="/cart">购物车</van-tabbar-item>
-      <van-tabbar-item icon="user-o" to="/user">我的</van-tabbar-item>
-    </van-tabbar>
-    <router-view/>
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
+
+    <router-view name="footer-bar"/>
   </div>
 </template>
 <script>
     export default {
-        data() {
-            return {
-                active: 0
-            }
-        }
+
     }
 </script>
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  /*text-align: center;*/
-  color: #2c3e50;
-  /*margin-top: 60px;*/
-}
 
 </style>
--- mall/src/main.js
+++ mall/src/main.js
@@ -3,15 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 import '@/assets/css/reset.css'
 import '@/assets/js/rem.js'
-import { Tabbar, TabbarItem ,Swipe, SwipeItem ,Lazyload ,NavBar ,Icon } from 'vant';
-Vue.use(Tabbar).use(TabbarItem).use(Swipe).use(SwipeItem).use(Lazyload).use(NavBar).use(Icon);
+//按需引用，
+import { Tabbar, TabbarItem ,Swipe, SwipeItem ,Lazyload ,NavBar ,Icon ,Tab, Tabs ,Field ,Cell, CellGroup,
+    Button,Toast,Row, Col,List,PullRefresh,GoodsAction,GoodsActionBigBtn,GoodsActionMiniBtn,Card,SubmitBar} from 'vant';
+Vue.use(Tabbar).use(TabbarItem).use(Swipe).use(SwipeItem).use(Lazyload).use(NavBar).use(Icon).use(Tab)
+    .use(Tabs).use(Field).use(Cell).use(CellGroup).use( Button).use(Toast).use(Row).use(Col).use(List)
+    .use(PullRefresh).use(GoodsAction).use(GoodsActionBigBtn).use(GoodsActionMiniBtn).use(Card).use(SubmitBar);
 
 import '@/mock/mock.js'
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
