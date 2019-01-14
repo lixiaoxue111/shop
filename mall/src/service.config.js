c156e80eb5daf7f4f47a3b3f006bae3f23d48a2f
newlls <523786175@qq.com>
2019-01-14 15:48:59 +0800

    服务端配置

--- mall/src/service.config.js
+++ mall/src/service.config.js
@@ -1,9 +1,15 @@
-const MOCKURL = 'http://www.nefuer.com/';
-const SERVER = '';
+const MOCKURL = 'http://www.nefuer.com/'; // mock模拟地址
+const SERVERURL = 'http://localhost:3000/'; //真实地址
 
 const URL = {
-    //getVarietyItem:MOCKURL + 'getVarietyItem'
     getVarietyItem: MOCKURL + 'getVarietyItem',
+    registUser: SERVERURL + 'user/registUser', //表示后台有一个user控制器，控制器下有一个registUser方法
+    loginUser: SERVERURL + 'user/loginUser',
+    getTypes: SERVERURL + 'type/getTypes',
+    getProductsByType: SERVERURL + 'product/getProductsByType',
+    getDetail: SERVERURL + 'product/getDetail',
+    addCart: SERVERURL + 'cart/addCart',
+    getCart: SERVERURL + 'cart/getCart'
 };
 
 export default URL;
